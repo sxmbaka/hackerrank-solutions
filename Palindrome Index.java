ab94b26606f7f7e060fb215c89977c6211882786
Yatharth Singh <96312006+yatharthw@users.noreply.github.com>
2022-12-08 20:46:07 +0530

    Two Pointer

---
+++ Palindrome Index.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'palindromeIndex' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int palindromeIndex(String s)
+    {
+        int l = s.length();
+        int i,j,a,b;
+        for (i=0, j=l-1; i<l; i++,j--){
+            if (s.charAt(i)!=s.charAt(j))
+                break;
+        }
+        if (i>j) return -1;
+
+        for (a = i+1, b = j;a<j && b>i+1; a++,b--){
+            if (s.charAt(a)!=s.charAt(b))
+                return j;
+        }
+        return i;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                int result = Result.palindromeIndex(s);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
